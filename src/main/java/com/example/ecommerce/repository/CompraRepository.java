3a29c661457668997285c3bfbe538acb586e5077
Bruni <bruniely.marchioro@zup.com.br>
2025-02-03 09:08:29 -0300

    Cria a interface CompraRepository que define os métodos para salvar, listar compras e listar compras por CPF

---
+++ src/main/java/com/example/ecommerce/repository/CompraRepository.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.Compra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompraRepository extends JpaRepository<Compra, Long> {
+    List<Compra> findByClienteCpf(String cpf);
+}
