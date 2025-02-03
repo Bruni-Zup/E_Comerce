7774f868e0905f77ea30488d2052de8189c39f9b
Bruni <bruniely.marchioro@zup.com.br>
2025-02-03 08:42:10 -0300

    Cria controlador ProdutoController para expor a API de produtos

---
+++ src/main/java/com/example/ecommerce/controller/ProdutoController.java
@@ -0,0 +1,30 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.model.Produto;
+import com.example.ecommerce.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    @Autowired
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping
+    public void cadastrarProduto(@RequestBody Produto produto) {
+        produtoService.cadastrarProduto(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listarProdutos() {
+        return produtoService.listarProdutos();
+    }
+}
