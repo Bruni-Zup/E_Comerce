74afbddd20ee22af68c351eadea759f6d9f47499
Bruni <bruniely.marchioro@zup.com.br>
2025-02-03 09:05:16 -0300

    Cria interface ProdutoRepository para persistência de produtos

---
+++ src/main/java/com/example/ecommerce/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Produto findByNome(String nome);
+    boolean existsByNome(String nome);
+}
