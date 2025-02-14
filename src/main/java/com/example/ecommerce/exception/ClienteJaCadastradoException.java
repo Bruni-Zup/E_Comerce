7f53c71bb32713c10d04d5eb88fd04334febf462
Bruni <bruniely.marchioro@zup.com.br>
2025-02-14 09:20:46 -0300

    cria classe ClienteJaCadastrado

--- src/main/java/com/example/ecommerce/exception/ClienteJaCadastradoException.java
+++ src/main/java/com/example/ecommerce/exception/ClienteJaCadastradoException.java
@@ -1,4 +1,10 @@
 package com.example.ecommerce.exception;
 
-public class ClienteJaCadastradoException {
+/**
+ * Exceção lançada quando se tenta cadastrar um cliente que já está cadastrado.
+ */
+public class ClienteJaCadastradoException extends RuntimeException {
+    public ClienteJaCadastradoException(String mensagem) {
+        super(mensagem);
+    }
 }
