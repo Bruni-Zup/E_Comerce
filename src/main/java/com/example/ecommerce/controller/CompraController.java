9e7069460b3881c5e3b4ccc5d698e644ea647cfb
Bruni <bruniely.marchioro@zup.com.br>
2025-02-14 09:18:36 -0300

    Melhora tratamento de erros e ajustes nos parâmetros de compra no CompraController

--- src/main/java/com/example/ecommerce/controller/CompraController.java
+++ src/main/java/com/example/ecommerce/controller/CompraController.java
@@ -1,10 +1,11 @@
 package com.example.ecommerce.controller;
 
 import com.example.ecommerce.model.Compra;
-import com.example.ecommerce.model.Produto;
 import com.example.ecommerce.service.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,16 +20,24 @@ public class CompraController {
         this.compraService = compraService;
     }
 
+    // Realizar uma compra - Recebe CPF e uma lista de IDs de produtos
     @PostMapping
-    public Compra realizarCompra(@RequestParam String cpf, @RequestBody List<Produto> produtos) {
-        return compraService.realizarCompra(cpf, produtos);
+    public Compra realizarCompra(@RequestParam String cpf, @RequestBody List<Long> produtoIds) {
+        try {
+            return compraService.realizarCompra(cpf, produtoIds);
+        } catch (Exception e) {
+            // Retorna um erro 400 caso algum produto esteja em falta ou outra exceção ocorra
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
+    // Listar todas as compras
     @GetMapping
     public List<Compra> listarCompras() {
         return compraService.listarCompras();
     }
 
+    // Listar compras por cliente - Recebe o CPF como parâmetro
     @GetMapping("/{cpf}")
     public List<Compra> listarComprasPorCliente(@PathVariable String cpf) {
         return compraService.listarComprasPorCliente(cpf);
