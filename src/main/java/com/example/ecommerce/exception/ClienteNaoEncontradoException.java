5cfb40ea18586cd985eba1f3c61f24654850139e
Bruni <bruniely.marchioro@zup.com.br>
2025-02-14 09:21:29 -0300

    cria classe cliente não encontrado

--- src/main/java/com/example/ecommerce/exception/ClienteNaoEncontradoException.java
+++ src/main/java/com/example/ecommerce/exception/ClienteNaoEncontradoException.java
@@ -1,4 +1,14 @@
 package com.example.ecommerce.exception;
 
-public class ClienteNaoEncontradoException {
+/**
+ * Exceção lançada quando um cliente não é encontrado na base de dados.
+ */
+public class ClienteNaoEncontradoException extends RuntimeException {
+    public ClienteNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+
+    public ClienteNaoEncontradoException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
 }
